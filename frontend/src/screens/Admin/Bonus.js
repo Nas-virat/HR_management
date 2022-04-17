5e817160c3e7dbf33c8bd4f880e1402ff134462b
Nas-virat <Napasvinitnantharat@gmail.com>
2022-04-17 13:37:45 +0700

    Bonus.js add handlesubmit and onchange

--- frontend/src/screens/Admin/Bonus.js
+++ frontend/src/screens/Admin/Bonus.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +12,25 @@ import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar';
 
 const Bonus = () => {
-  return (
+
+
+    const [employeeid, setEmployeeId] = useState('');
+    const [bonus, setBonus] = useState(0);
+    const [adminid, setAdminId] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({'EmployeeId' :employeeid, 
+                    'Bonus Amount':bonus, 
+                    'AdminId': adminid
+                    });
+        alert({'EmployeeId' :employeeid, 
+        'Bonus Amount':bonus, 
+        'AdminId': adminid
+        });
+    }
+
+    return (
     <div>
         <Navbar />
         <Sidebar />
@@ -21,19 +40,19 @@ const Bonus = () => {
                 <Form> 
                     <Form.Group className="mb-3 " controlId="formBasicEmail">
                         <Form.Label>Employee ID</Form.Label>
-                        <Form.Control className = "inputform" type="text" placeholder="Enter Employee ID"/>
+                        <Form.Control className = "inputform" type="text" placeholder="Enter Employee ID" defaultValue={employeeid || ""} onChange= {e=> setEmployeeId(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3 " controlId="formBasicEmail">
                         <Form.Label>Bonus Amount</Form.Label>
-                        <Form.Control className = "inputform" type="number" placeholder="Enter Bonus Amount" />
+                        <Form.Control className = "inputform" type="number" placeholder="Enter Bonus Amount" defaultValue={bonus || ""} onChange= {e=> setBonus(e.target.value)}/>
                     </Form.Group>
                     
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Admin ID</Form.Label>
-                        <Form.Control className = "inputform" type="text" placeholder="Enter Admin ID" />
+                        <Form.Control className = "inputform" type="text" placeholder="Enter Admin ID" defaultValue={adminid || ""} onChange= {e=> setAdminId(e.target.value)}/>
                     </Form.Group>
-                    <Button variant="success" type="submit">
+                    <Button variant="success" type="submit" onClick={handleSubmit}>
                         Submit
                     </Button>
                 </Form>
