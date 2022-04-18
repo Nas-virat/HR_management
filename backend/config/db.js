ce556b7e5ccaa707e2da416d460d196d89706acd
Nas-virat <Napasvinitnantharat@gmail.com>
2022-04-18 11:19:27 +0700

    .env db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"",
-    database:"dreamhome",
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
     insecureAuth : true
 });
 
