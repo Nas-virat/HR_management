2fa17ad7c77b52a32dc2d27ac3c96d5612903afd
Nas-virat <Napasvinitnantharat@gmail.com>
2022-04-30 08:42:26 +0700

    edit bug on server uploads dirname

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require('express');
 const cors = require("cors");
 const multer = require('multer');
+const path = require('path');
 
 
 const { getAllDepartment,
@@ -53,7 +54,7 @@ app.use('/uploads', express.static('uploads'));
 
 const storage = multer.diskStorage({
   destination: function(req, file, callback) {
-    callback(null, __dirname);
+    callback(null, path.join(__dirname, 'uploads'));
   },
   filename: function (req, file, callback) {
     callback(null, Date.now() + "-" +file.originalname);
